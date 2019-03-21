7a516ea1e004c27c96fbd2cd3e1fc8709257adb0
sylwek <sylwester.kalinowski@outlook.com>
2019-03-21 20:27:48 +0100

    Maven learning

---
+++ maven/kino-dao/src/main/java/pl/sylwek/kino/dao/SeansDAO.java
@@ -0,0 +1,9 @@
+package pl.sylwek.kino.dao;
+
+import pl.sylwek.kino.entities.Seans;
+
+import com.googlecode.genericdao.dao.hibernate.GenericDAO;
+
+public interface SeansDAO extends GenericDAO<Seans, Long> {
+
+}
---
+++ maven/kino-services/src/main/java/pl/sylwek/kino/services/Test.java
@@ -0,0 +1,5 @@
+package pl.sylwek.kino.services;
+
+public class Test {
+	
+}
